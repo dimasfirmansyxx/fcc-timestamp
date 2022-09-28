ae095366050ba10f02ba0aadcb3294f0c41bc529
Dimas Nasution <dimasfirmansyxx@gmail.com>
2022-09-28 14:10:43 +0700

    use arrow function

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ app.use(cors({optionsSuccessStatus: 200}))
 
 app.use(express.static('public'))
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 })
 
-app.get('/api/hello', function (req, res) {
+app.get('/api/hello', (req, res) => {
   res.json({greeting: 'hello API'})
 })
 
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
